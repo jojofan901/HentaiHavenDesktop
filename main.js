92bc10c40fbe271ca801c9861d11f619c0c5325e
jojofan901 <77402212+jojofan901@users.noreply.github.com>
2021-01-19 12:44:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const electroon = require('electron');
+
 console.log('uhhh I dont have shit done yet UwU');
 
 console.log('btw no hentai in this project *wink* *wink* thats all somewhere else. idk if thid will get me removed from git hib but...');
