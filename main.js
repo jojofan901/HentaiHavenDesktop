8c57f308c145bb05aa030e8e2bd2fcc3360733c4
jojofan901 <77402212+jojofan901@users.noreply.github.com>
2021-01-19 11:54:36 -0600

    Adding the file that will fuck me over someday...
    Hopefully companies won’t find this.

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('uhhh I dont have shit done yet UwU');
+
+console.log('btw no hentai in this project *wink* *wink* thats all somewhere else. idk if thid will get me removed from git hib but...');
+
+console.log('but for yall who want to see hentai I got you');
+
+console.log('8==================D ()');
+
+console.log('you fucking pervert');
